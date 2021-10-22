026e09345c2c9c6823a5704233478729c9a8e14e
theines <churn8282@naver.com>
2021-10-22 16:22:31 +0900

    classes on Typescript

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,23 @@
 "use strict";
+/*
+    interface는 js가 아니라서 자주는 아니지만 js에 interface를 넣고싶을 때
+    어떻게 하냐 -> class!
+    매번 클래스 하는게 아니라 어떨 때는 인터페이스만 필요하고 어떤때는 클래스가 필요없는데
+    interface로만 하는게 ts측면에서 좀 더 안전하다.
+    react, express, node 를 사용한다면 class가 필요하다.
+*/
 Object.defineProperty(exports, "__esModule", { value: true });
-const name = "Jiyoung", age = 24, gender = "female";
-const sayHi = (name, age, gender) => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+// ts에서는 class가 어떤 속성들을 가져야 하는지 선언해야 해(그리고 속성들의 권한도)
+class Human {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+const lynn = new Human("Lynn", 18, "female");
+const sayHi = (person) => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 };
-console.log(sayHi("Jiyoung", 22, "female"));
+console.log(sayHi(lynn));
 //# sourceMappingURL=index.js.map
